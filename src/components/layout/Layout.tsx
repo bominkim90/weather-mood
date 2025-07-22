19ec8dfb9685247d22713be863a4c3fdd93b1995
bominkim90 <bomin97@naver.com>
2025-07-22 23:30:02 +0900

    chore: Vite환경에 tailwind CSS 환경 구축
    - import오류 발생 : @import 'tailwindcss' 다음줄에 @import url('구글폰트') 이렇게 하니까
    @import가 먼저 와야된다고 터미널에서 오류가 뜸
    - 그래서 그냥 @import url()이걸 위로 바꿨더니 해결됨. 먼가 tailwind에서 정해놓은 규칙이 다른걸 호환을 못하나봄

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router-dom';
 
 export default function Layout() {
   return (
-    <div>
+    <div className="absolute top-0 bottom-0 left-0 right-0 max-w-[480px] mx-auto bg-white shadow-md">
       <Outlet />
       <NavBar />
     </div>
