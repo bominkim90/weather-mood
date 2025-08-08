96426c90f35453a2369b0ba120aeb4d2175662ef
bominkim90 <bomin97@naver.com>
2025-08-08 18:09:14 +0900

    fix: refresh토큰 무한 재요청 수정
    ## 추가 작업
    - 전역 스토어 값으로 location 저장 안되는 점 수정 => localStorage에 저장으로 수정

--- src/store/useUserLocationStore.ts
+++ src/store/useUserLocationStore.ts
@@ -1,24 +1,24 @@
 import { create } from 'zustand';
-// import { persist } from 'zustand/middleware';
+import { persist } from 'zustand/middleware';
 
 interface UserLocationState {
   locationStore: string;
   setLocationStore: (location: string) => void;
 }
 
-export const useUserLocationStore = create<UserLocationState>((set) => ({
-  locationStore: '',
-  setLocationStore: (locationStore: string) => set({ locationStore }),
-}));
+// export const useUserLocationStore = create<UserLocationState>((set) => ({
+//   locationStore: '',
+//   setLocationStore: (locationStore: string) => set({ locationStore }),
+// }));
 
-// export const useUserLocationStore = create<UserLocationState>()(
-//   persist(
-//     (set) => ({
-//       locationStore: '',
-//       setLocationStore: (locationStore: string) => set({ locationStore }),
-//     }),
-//     {
-//       name: 'user-location-storage',
-//     }
-//   )
-// );
+export const useUserLocationStore = create<UserLocationState>()(
+  persist(
+    (set) => ({
+      locationStore: '',
+      setLocationStore: (locationStore: string) => set({ locationStore }),
+    }),
+    {
+      name: 'user-location-storage',
+    }
+  )
+);
